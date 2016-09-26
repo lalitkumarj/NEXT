d30895bf37dade2af2c888fc4f9dd75e287a9567
Lalit Jain <lalitkumarj@gmail.com>
2016-09-26 12:24:07 -0400

    Update ActiveRanking.py

--- apps/ActiveRanking/ActiveRanking.py
+++ apps/ActiveRanking/ActiveRanking.py
@@ -69,8 +69,7 @@ class ActiveRanking(object):
         #    targets_list[0]['flag'] = 0
         #    targets_list[1]['flag'] = 1
 
-        return_dict = {'target_indices':targets_list}
-
+        return_dict = {'target_indices':targets_list, 'quicksortid':alg_response[2]}
         experiment_dict = butler.experiment.get()
 
         #if 'labels' in experiment_dict['args']['rating_scale']:
@@ -87,13 +86,14 @@ class ActiveRanking(object):
         targets = query['target_indices']
         left_id = target[0]['target']
         right_id = target[1]['target']
-                
+        quicksortid = query['quicksortid']
+        
         winner_id = args['target_winner']
         butler.experiment.increment(key='num_reported_answers_for_' + query['alg_label'])
 
         alg({'left_id':left_id, 
              'right_id':right_id, 
-             'winner_id':winner_id})
+             'winner_id':winner_id, 'quicksortid':quicksortid})
         return {'winner_id':winner_id}
                 
 
